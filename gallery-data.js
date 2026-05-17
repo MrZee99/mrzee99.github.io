d14aec07fb58a31a79634aac5c05902ca593fd79
MrZee99 <mrzee99@gmail.com>
2026-05-17 16:12:28 -0600

    Add gallery image manifest

---
+++ gallery-data.js
@@ -0,0 +1,23 @@
+const galleryItems = [
+  // Add images here using this format:
+  // { project: "Project Name", folder: "Folder-Name", file: "image-name.png" },
+
+  // Example entries below. Replace/add to these with your actual PNG filenames.
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "fake.png" },
+  { project: "Club Luve", folder: "Club-Luve", file: "fake.png" },
+  { project: "Metro", folder: "Metro", file: "fake.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "fake.png" },
+  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "fake.png" },
+  { project: "Slav", folder: "Slav", file: "fake.png" },
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "fake.png" },
+  { project: "Weapons", folder: "Weapons", file: "fake.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "53.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "54.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "55.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "56.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "57.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "58.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "59.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "60.png" },
+  { project: "Z.E.E", folder: "ZEE", file: "61.png" }
+];
