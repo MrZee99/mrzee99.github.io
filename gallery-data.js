22376eb7c96ad226831f5ab6503621e2968d532f
MrZee99 <mrzee99@gmail.com>
2026-05-17 16:17:40 -0600

    Populate gallery manifest with actual image filenames

--- gallery-data.js
+++ gallery-data.js
@@ -1,16 +1,52 @@
 const galleryItems = [
-  // Add images here using this format:
-  // { project: "Project Name", folder: "Folder-Name", file: "image-name.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "43.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "44.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "45.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "46.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "47.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "48.png" },
+  { project: "Club Cyberia", folder: "Club-Cyberia", file: "49.png" },
 
-  // Example entries below. Replace/add to these with your actual PNG filenames.
-  { project: "Club Cyberia", folder: "Club-Cyberia", file: "fake.png" },
-  { project: "Club Luve", folder: "Club-Luve", file: "fake.png" },
-  { project: "Metro", folder: "Metro", file: "fake.png" },
-  { project: "Misc Practice", folder: "Misc-Practice", file: "fake.png" },
-  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "fake.png" },
-  { project: "Slav", folder: "Slav", file: "fake.png" },
-  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "fake.png" },
-  { project: "Weapons", folder: "Weapons", file: "fake.png" },
+  { project: "Club Luve", folder: "Club-Luve", file: "10.png" },
+  { project: "Club Luve", folder: "Club-Luve", file: "11.png" },
+  { project: "Club Luve", folder: "Club-Luve", file: "12.png" },
+  { project: "Club Luve", folder: "Club-Luve", file: "13.png" },
+
+  { project: "Metro", folder: "Metro", file: "34.png" },
+
+  { project: "Misc Practice", folder: "Misc-Practice", file: "1.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "15.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "17.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "18.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "26.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "31.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "32.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "35.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "36.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "42.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "50.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "51.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "6.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "7.png" },
+  { project: "Misc Practice", folder: "Misc-Practice", file: "9.png" },
+
+  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "25.png" },
+  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "29.png" },
+  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "39.png" },
+  { project: "Photo Manipulation", folder: "Photo-Manipulation", file: "95c-Recovered.png" },
+
+  { project: "Slav", folder: "Slav", file: "23.png" },
+
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "2.png" },
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "4.png" },
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "40.png" },
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "41.png" },
+  { project: "Tyler Chibi", folder: "Tyler-Chibi", file: "8.png" },
+
+  { project: "Weapons", folder: "Weapons", file: "22.png" },
+  { project: "Weapons", folder: "Weapons", file: "3.png" },
+  { project: "Weapons", folder: "Weapons", file: "37.png" },
+
   { project: "Z.E.E", folder: "ZEE", file: "53.png" },
   { project: "Z.E.E", folder: "ZEE", file: "54.png" },
   { project: "Z.E.E", folder: "ZEE", file: "55.png" },
